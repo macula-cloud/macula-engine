d62716493e5e97793d97e39e127aec933148c519
Jokeway <jokeway@hotmail.com>
2020-07-01 18:50:32 +0800

    [IMP] set GivenDomainId as default

--- macula-cloud-starter-core/src/main/java/org/macula/cloud/core/domain/AbstractPersistable.java
+++ macula-cloud-starter-core/src/main/java/org/macula/cloud/core/domain/AbstractPersistable.java
@@ -2,16 +2,101 @@ package org.macula.cloud.core.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.domain.Persistable;
-
-import lombok.ToString;
+import org.springframework.data.util.ProxyUtils;
+import org.springframework.lang.Nullable;
 
 @MappedSuperclass
-@ToString(callSuper = true)
-public abstract class AbstractPersistable<PK extends Serializable> extends org.springframework.data.jpa.domain.AbstractPersistable<PK>
+public abstract class AbstractPersistable<PK extends Serializable> // extends org.springframework.data.jpa.domain.AbstractPersistable<PK>
 		implements Persistable<PK> {
+
+	@GeneratedValue(generator = "GivenDomainId")
+	@GenericGenerator(name = "GivenDomainId", strategy = "org.macula.cloud.core.domain.GivenDomainId")
+	@Id
+	private @Nullable PK id;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.domain.Persistable#getId()
+	 */
+	@Nullable
+	@Override
+	public PK getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id of the entity.
+	 *
+	 * @param id the id to set
+	 */
+	protected void setId(@Nullable PK id) {
+		this.id = id;
+	}
+
+	/**
+	 * Must be {@link Transient} in order to ensure that no JPA provider complains because of a missing setter.
+	 *
+	 * @see org.springframework.data.domain.Persistable#isNew()
+	 */
+	@Transient // DATAJPA-622
+	@Override
+	public boolean isNew() {
+		return null == getId();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return String.format("Entity of type %s with id: %s", this.getClass().getName(), getId());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+
+		if (null == obj) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (!getClass().equals(ProxyUtils.getUserClass(obj))) {
+			return false;
+		}
+
+		AbstractPersistable<?> that = (AbstractPersistable<?>) obj;
+
+		return null == this.getId() ? false : this.getId().equals(that.getId());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+
+		int hashCode = 17;
+
+		hashCode += null == getId() ? 0 : getId().hashCode() * 31;
+
+		return hashCode;
+	}
 
 	/** 是否已删除标志 */
 	private transient boolean deleted = false;
@@ -39,4 +124,5 @@ public abstract class AbstractPersistable<PK extends Serializable> extends org.s
 	public void cloneId(AbstractPersistable<PK> entity) {
 		entity.setId(getId());
 	}
+
 }
