714a147cc2cdd37a5fcfebc25aca6cc581e47b3d
Jokeway <jokeway@hotmail.com>
2020-07-02 09:53:54 +0800

    [IMP] ignore oauth2 feign interceptor errors

--- macula-cloud-starter-security/src/main/java/org/macula/cloud/security/feign/OAuth2FeignRequestInterceptor.java
+++ macula-cloud-starter-security/src/main/java/org/macula/cloud/security/feign/OAuth2FeignRequestInterceptor.java
@@ -5,8 +5,10 @@ import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResour
 
 import feign.MethodMetadata;
 import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
 
 @SuppressWarnings("deprecation")
+@Slf4j
 public class OAuth2FeignRequestInterceptor extends org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor {
 
 	public OAuth2FeignRequestInterceptor(OAuth2ClientContext oAuth2ClientContext, OAuth2ProtectedResourceDetails resource) {
@@ -23,7 +25,11 @@ public class OAuth2FeignRequestInterceptor extends org.springframework.cloud.sec
 		if (template.headers().containsKey(AUTHORIZATION)) {
 			return;
 		}
-		super.apply(template);
+		try {
+			super.apply(template);
+		} catch (Exception ex) {
+			log.error("Apply OAuth2FeignRequest error: ", ex);
+		}
 	}
 
 }
