ff6caf3c464974ccfa439db2f5a0d75c5e07a289
Jokeway <jokeway@hotmail.com>
2020-07-16 11:29:31 +0800

    [IMP] update MQ settings

--- macula-cloud-starter-mq/src/main/java/org/macula/cloud/mq/configure/AliMQConfig.java
+++ macula-cloud-starter-mq/src/main/java/org/macula/cloud/mq/configure/AliMQConfig.java
@@ -1,10 +1,9 @@
 package org.macula.cloud.mq.configure;
 
+import java.util.Map;
 import java.util.Properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import com.aliyun.openservices.ons.api.PropertyKeyConst;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,34 +13,11 @@ import lombok.Setter;
 @ConfigurationProperties(prefix = "macula.cloud.alimq")
 public class AliMQConfig {
 
-	/**
-	* MQ 接入参数 accessKey
-	*/
-	private String accessKey;
+	private Map<String, String> props;
 
-	/**
-	 * MQ 接入参数 secretKey
-	 */
-	private String secretKey;
-
-	/**
-	 * MQ 的接入域名
-	 */
-	private String onsAddr;
-
-	/**
-	 * 分组ID
-	 */
-	private String groupId;
-
-	@SuppressWarnings("deprecation")
 	public Properties getProperties() {
 		Properties properties = new Properties();
-		properties.put(PropertyKeyConst.AccessKey, accessKey);
-		properties.put(PropertyKeyConst.SecretKey, secretKey);
-		properties.put(PropertyKeyConst.ONSAddr, onsAddr);
-		properties.put(PropertyKeyConst.ConsumerId, groupId);
-		properties.put(PropertyKeyConst.ProducerId, groupId);
+		properties.putAll(props);
 		return properties;
 	}
 }
