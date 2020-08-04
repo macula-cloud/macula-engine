42f845e1acd865e31c6d4ce31e7a30636b32afa6
Jokeway <jokeway@hotmail.com>
2020-08-04 14:08:04 +0800

    [FIX] remove datasource transaction managerment

--- macula-cloud-starter-br/src/main/java/org/macula/cloud/br/configure/ChoerodonConfiguration.java
+++ macula-cloud-starter-br/src/main/java/org/macula/cloud/br/configure/ChoerodonConfiguration.java
@@ -1,32 +1,26 @@
 package org.macula.cloud.br.configure;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass({ JdbcTemplate.class, PlatformTransactionManager.class })
-@AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
+@AutoConfigureAfter(JpaRepositoriesAutoConfiguration.class)
 @EnableConfigurationProperties(DataSourceProperties.class)
 public class ChoerodonConfiguration {
 
-	@Bean
-	DataSourceTransactionManager transactionManager(DataSource dataSource,
-			ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
-		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
-		transactionManagerCustomizers.ifAvailable((customizers) -> customizers.customize(transactionManager));
-		return transactionManager;
-	}
+	//	@Bean
+	//	DataSourceTransactionManager transactionManager(DataSource dataSource,
+	//			ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
+	//		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+	//		transactionManagerCustomizers.ifAvailable((customizers) -> customizers.customize(transactionManager));
+	//		return transactionManager;
+	//	}
 
 }
--- macula-cloud-starter-log/src/main/java/org/macula/cloud/log/configure/ServiceInvokeProxyAutoConfiguration.java
+++ macula-cloud-starter-log/src/main/java/org/macula/cloud/log/configure/ServiceInvokeProxyAutoConfiguration.java
@@ -5,14 +5,18 @@ import org.macula.cloud.log.interceptor.ServiceInvokeLogService;
 import org.macula.cloud.log.repository.ServiceInvokeLogRepository;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @AutoConfigureOrder(AutoConfigureOrder.DEFAULT_ORDER + 100)
 @EnableAspectJAutoProxy
 @ConditionalOnProperty(prefix = "macula.cloud.invokelog", name = "enabled", havingValue = "true", matchIfMissing = true)
+@EnableJpaRepositories(basePackages = "org.macula.cloud.log.repository")
+@EntityScan(basePackages = "org.macula.cloud.log.domain")
 public class ServiceInvokeProxyAutoConfiguration {
 
 	@Bean
