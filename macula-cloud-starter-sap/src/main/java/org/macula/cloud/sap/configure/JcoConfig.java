59bbd195a0a799b3368dbe9ce55d6e6e9f6a30b2
Jokeway <jokeway@hotmail.com>
2020-10-14 17:32:02 +0800

    [IMP] restore SAP module config

--- macula-cloud-starter-sap/src/main/java/org/macula/cloud/sap/configure/JcoConfig.java
+++ macula-cloud-starter-sap/src/main/java/org/macula/cloud/sap/configure/JcoConfig.java
@@ -1,9 +1,7 @@
 package org.macula.cloud.sap.configure;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -16,7 +14,7 @@ import lombok.Setter;
 public class JcoConfig {
 
 	private String name;
-	private List<String> packages = new ArrayList<String>();
-	private Map<String, String> props = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+	private List<String> packages;
+	private Map<String, String> props;
 
 }
