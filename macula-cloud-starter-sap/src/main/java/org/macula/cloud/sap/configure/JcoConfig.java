4d855c3091194c7d1f16b98cf38df5266665a17b
Jokeway <jokeway@hotmail.com>
2020-10-14 16:41:16 +0800

    [IMP] add initialize

--- macula-cloud-starter-sap/src/main/java/org/macula/cloud/sap/configure/JcoConfig.java
+++ macula-cloud-starter-sap/src/main/java/org/macula/cloud/sap/configure/JcoConfig.java
@@ -1,7 +1,9 @@
 package org.macula.cloud.sap.configure;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -14,7 +16,7 @@ import lombok.Setter;
 public class JcoConfig {
 
 	private String name;
-	private List<String> packages;
-	private Map<String, String> props;
+	private List<String> packages = new ArrayList<String>();
+	private Map<String, String> props = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
 
 }
